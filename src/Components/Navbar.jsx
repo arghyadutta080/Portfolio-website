c16f5057f0e89af683688718c1c6f6dd9aa7ef66
Arghya <arghyadutta080@gmail.com>
2023-10-09 03:42:37 +0530

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,87 @@
+import { useState } from 'react'
+import { Dialog, Popover } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon, } from '@heroicons/react/24/outline'
+import '../App.css'
+
+
+export default function Navbar() {
+
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+    return (
+        <header className=" z-10">
+            <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-10 " aria-label="Global">
+                <div className="flex lg:flex-1">
+                    <a href="#" className=" p-1.5">
+                        <span className="text-lg font-semibold leading-6 text-white">Arghya Dutta</span>
+                    </a>
+                </div>
+                <div className="flex lg:hidden">
+                    <button
+                        type="button"
+                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                        onClick={() => setMobileMenuOpen(true)}>
+                        <span className="sr-only">Open main menu</span>
+                        <Bars3Icon className="h-9 w-9 text-white" aria-hidden="true" />
+                    </button>
+                </div>
+                <Popover.Group className="hidden lg:flex lg:gap-x-12">
+                    <a href="#" className="text-lg font-semibold leading-6 text-white">
+                        About
+                    </a>
+                    <a href="#" className="text-lg font-semibold leading-6 text-white">
+                        Education
+                    </a>
+                    <a href="#" className="text-lg font-semibold leading-6 text-white">
+                        Experience
+                    </a>
+                    <a href="#" className="text-lg font-semibold leading-6 text-white">
+                        Projects
+                    </a>
+                    <a href="#" className="text-lg font-semibold leading-6 text-white">
+                        Contact
+                    </a>
+                </Popover.Group>
+            </nav>
+            <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+                <div className="fixed inset-0 " />
+                <Dialog.Panel className="fixed bg-page inset-y-0 right-0 w-full overflow-y-auto px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+                >
+                    <div className="flex items-center justify-between">
+                        <a href="#" className="-m-1.5 p-1.5">
+                            <span className="text-lg font-semibold leading-6 text-white">Arghya Dutta</span>
+                        </a>
+                        <button
+                            type="button"
+                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                            onClick={() => setMobileMenuOpen(false)}>
+                            <span className="sr-only">Close menu</span>
+                            <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                        </button>
+                    </div>
+                    <div className="mt-6 flow-root ">
+                        <div className="-my-6 divide-y divide-gray-500/10 ">
+                            <div className="space-y-2 py-6">
+                                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white">
+                                    About
+                                </a>
+                                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white">
+                                    Education
+                                </a>
+                                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white">
+                                    Experience
+                                </a>
+                                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white">
+                                    Projects
+                                </a>
+                                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white">
+                                    Contact
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </Dialog.Panel>
+            </Dialog>
+        </header>
+    )
+}
